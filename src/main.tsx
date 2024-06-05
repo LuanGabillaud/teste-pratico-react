ee439ea977c7d16149b8e9efb5cdb7d1d90da2fa
Luan Gabillaud <calazansgabillaud@gmail.com>
2024-06-05 15:20:47 -0300

    Feat: Incluindo as rotas

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//Pages
+import Home from './routes/Home.tsx'
+import Dashboard from './routes/Dashboard.tsx'
+import Login from './routes/Login.tsx'
+import Signin from './routes/Signin.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/login",
+        element: <Login/>
+      },
+      {
+        path: "/cadastre-se",
+        element: <Signin/>
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard/>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
